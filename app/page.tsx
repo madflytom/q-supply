f39a4bdf5ce5e29e00f9790cb7f85b6bd83aba1f
Tom Stahl <madflytom@gmail.com>
2024-07-14 16:53:30 -0400

    Simple CRUD actions implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,61 @@
-const Home = () => {
-  return <h2>Home</h2>;
+import { revalidatePath } from 'next/cache';
+import { prisma } from '@/lib/prisma';
+import Link from 'next/link';
+
+const createPost = async (formData: FormData) => {
+  'use server';
+
+  const name = formData.get('name') as string;
+
+  await prisma.post.create({
+    data: {
+      name,
+    },
+  });
+
+  revalidatePath('/');
+};
+
+const deletePost = async (id: string) => {
+  'use server';
+
+  await prisma.post.delete({
+    where: {
+      id,
+    },
+  });
+
+  revalidatePath('/');
+};
+
+const Home = async () => {
+  const posts = await prisma.post.findMany();
+
+  return (
+    <div className="p-4 flex flex-col gap-y-4">
+      <h2>Home</h2>
+
+      <form action={createPost} className="flex flex-col gap-y-2">
+        <input type="text" name="name" placeholder="Name" />
+        <button type="submit">Create</button>
+      </form>
+
+      <ul className="flex flex-col gap-y-2">
+        {posts.map((post) => (
+          <li key={post.id} className="flex items-center gap-x-4">
+            <div>{post.name}</div>
+            <div className="flex items-center">
+              <Link href={`/posts/${post.id}`}>Go To</Link> |{' '}
+              <Link href={`/posts/${post.id}/edit`}>Edit</Link>
+              <form action={deletePost.bind(null, post.id)}>
+                <button type="submit">Delete</button>
+              </form>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
